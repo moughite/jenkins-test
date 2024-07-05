268d095fc0be1b81e69edbe4ee1bd1c7da407432
moughite <40766861+moughite@users.noreply.github.com>
2024-07-05 15:51:27 +0200

    some change

--- src/main/java/com/jenkins/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/jenkins/jenkinsdemo/JenkinsdemoApplication.java
@@ -15,7 +15,7 @@ public class JenkinsdemoApplication {
 
 	@GetMapping("/hello")
 	public String getMessage(){
-		return " hello message teste!!!!!!!!!!!!!ß" ;
+		return " hello message teste" ;
 	}
 
 }
